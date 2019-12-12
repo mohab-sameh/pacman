41466a7e9e4329c5ed4085e007b4a0754de99531
WhiteArsenic <whitearsenic@outlook.com>
2019-12-12 21:02:24 +0200

    Add project files.

---
+++ Pacman/Classes/Audio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Pacman
+{
+    public class Audio
+    {
+        private const int players = 20;
+        private SoundPlayer[] player = new SoundPlayer[players];
+
+        public Audio()
+        {
+            for (int x=0; x<players; x++)
+            {
+                player[x] = new SoundPlayer();
+                player[x].Stream = Properties.Resources.audio1;
+            }
+        }
+
+        public void Play(int num)
+        {
+            player[num].Play();
+        }
+    }
+}
